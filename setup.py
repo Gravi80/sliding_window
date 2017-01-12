a3d71d32fc4fbc728abbab7f45c74e9950407f51
Ravi Sharma <ravis4376@gmail.com>
2017-01-13 00:13:57 +0530

    added setup file for uploading it to pypi.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='sliding_window',
+    packages=['sliding_window'],
+    version='0.1',
+    description='A lib to implement sliding window with overlapping',
+    author='Ravi Sharma',
+    author_email='ravi.sharma.cs11@gmail.com',
+    url='https://github.com/ravi2020/sliding_window',
+    download_url='https://github.com/ravi2020/sliding_window/tarball/0.1',
+    keywords=['window', 'sliding', 'overlap'],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research/Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Software Development :: Libraries :: Python Modules'
+    ]
+)
