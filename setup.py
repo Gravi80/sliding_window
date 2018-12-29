9fb388bf84acfffa46664df4046bc7c56c1434b3
Ravi Sharma <ravis4376@gmail.com>
2018-12-29 23:36:08 +0530

    fixing long description

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
+import pathlib
 from os import path
+
 from setuptools import setup, find_packages
+
+ROOT_DIR = pathlib.Path(__file__).parent
+README = (ROOT_DIR / "README.md").read_text()
 
 
 def read_file(filename):
@@ -10,9 +15,9 @@ def read_file(filename):
 setup(
     name='window_slider',
     packages=find_packages(exclude=['tests']),
-    version='0.7',
+    version='0.8',
     description='A lib to implement sliding window with overlapping on numpy array',
-    long_description=read_file('README.md'),
+    long_description=README,
     long_description_content_type="text/markdown",
     author='Ravi Sharma',
     author_email='ravi.sharma.cs11@gmail.com',
